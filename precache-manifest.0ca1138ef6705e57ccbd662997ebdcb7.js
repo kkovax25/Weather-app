5f95435d68b7c7699f248cf09dd601c8788f77f0
kkovax25 <kkovax94@gmail.com>
2020-05-06 14:57:37 +0200

    Updates

---
+++ precache-manifest.0ca1138ef6705e57ccbd662997ebdcb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e726d10b9b28c45e4c9141e666b43554",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "01e7c3a7227b5070c3b2",
+    "url": "/Weather-app/static/css/main.29ae145b.chunk.css"
+  },
+  {
+    "revision": "e27aea44d711328a563d",
+    "url": "/Weather-app/static/js/2.2ed2a2d0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Weather-app/static/js/2.2ed2a2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e7c3a7227b5070c3b2",
+    "url": "/Weather-app/static/js/main.7a19115b.chunk.js"
+  },
+  {
+    "revision": "4aafc8c22c987fdbe92a",
+    "url": "/Weather-app/static/js/runtime-main.35f062c4.js"
+  },
+  {
+    "revision": "f7e10555b313890fd1d946bf36c2c111",
+    "url": "/Weather-app/static/media/desktopBg.f7e10555.jpg"
+  }
+]);
